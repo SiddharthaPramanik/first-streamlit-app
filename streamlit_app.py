bc29dbeb479565908a4fc12e517743cdfa6e92d4
Siddhartha Pramanik <rks.siddhartha@gmail.com>
2022-10-18 17:51:42 +0530

    Update streamlit_app.py
    Added connection close and updated UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,14 +23,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header('View Our Fruit List - Add Your Favorites')
+
 # Fruityvice Section
 
 # Write function to fetch fruityvice data
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + this_fruit_choice)
   return pd.json_normalize(fruityvice_response.json())
-
-streamlit.header('Fruityvice Fruit Advice!')
 
 try:
   # User input
@@ -59,16 +59,18 @@ def add_fruit_to_load_list(new_fruit):
     return "Thanks for adding " + new_fruit
 
 # Add a button to load the fruits
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 # Add a button to add fruit to the load list
 add_my_fruit = streamlit.text_input('What fruit would you like add?')
-if streamlit.button('Add Fruit to Load List'):
+if streamlit.button('Add Fruit to List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   fruit_add_response = add_fruit_to_load_list(add_my_fruit)
+  my_cnx.close()
   streamlit.text(fruit_add_response)
 
 
